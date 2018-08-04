a16a233919beea9f4ea2f6a89e8e1981e382c2a2
Wang wuquan <wuquan.wang@hand-china.com>
2018-08-04 16:43:25 +0800

    部分代码修改

--- TestProject/src/main/java/ListAndMap.java
+++ TestProject/src/main/java/ListAndMap.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -17,44 +18,37 @@ public class ListAndMap {
     public static void main(String args[]) {
         //生成随机数
         GetRandomNumber();
-        //将数组值放入map并且遍历
-        addItemToMap();
+        //初始化map
+        AddItemToMap();
+        //打印输出
+        PrintingResult();
         //排序并且打印输出
         OutputSortData();
     }
 
-    private static void OutputSortData() {
-        System.out.print("排序后数据为:");
-        Set set2 = map.keySet();
-        Iterator it2 = set.iterator();
-        while (it2.hasNext()) {
-            int key = (Integer) it2.next();
-            System.out.print(key + "=>");
-            int[] value = (int[]) map.get(key);
 
-            for (int i = 0; i < 10; i++) {
-                int temp = 0;
-                for (int k = 0; k < value.length - 1; k++) {
-                    for (int m = k + 1; m < value.length; m++) {
-                        if (value[k] > value[m]) {
-                            temp = value[k];
-                            value[k] = value[m];
-                            value[m] = temp;
-                        }
-                    }
-                }
-                if (value[i] != 0)
-                    System.out.print(value[i] + " ");
-            }
-            System.out.println();
+
+    //生成随机数函数
+    private static void GetRandomNumber() {
+        System.out.print("随机生成50个小于100的数,分别为：");
+        for (int i = 0; i < 50; i++) {
+            int ii = (int) ((Math.random()) * 100);
+            list.add(ii);
         }
+        //打印
+        for (int i = 0; i < list.size()-1; i++) {
+            System.out.print(list.get(i) + ",");
+        }
+        System.out.println(list.get(list.size()-1));
     }
 
-    private static void addItemToMap() {
+
+    //初始化map
+    private static void AddItemToMap() {
         for (int i = 0; i < 10; i++) {
             int array[] = new int[10];
             int num = 0;
-            for (int j = 0; j < 50; j++) {
+            for (int j = 0; j < list.size(); j++) {
                 if (list.get(j) / 10 == i) {
                     array[num] = list.get(j);
                     num++;
@@ -62,16 +56,18 @@ public class ListAndMap {
             }
             map.put(i, array);
         }
-        System.out.println();
-        System.out.print("Map原数据为:");
+    }
+
+    //输出原始结果
+    private static void PrintingResult() {
+        System.out.println("Map原数据为:");
         set = map.keySet();
         Iterator it = set.iterator();
         while (it.hasNext()) {
             int key = (Integer) it.next();
             System.out.print(key + "=>");
             int[] value = (int[]) map.get(key);
-
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < value.length; i++) {
                 if (value[i] != 0)
                     System.out.print(value[i] + " ");
             }
@@ -79,16 +75,23 @@ public class ListAndMap {
         }
     }
 
-    //生成随机数函数
-    private static void GetRandomNumber() {
-        System.out.print("随机生成50个小于100的数,分别为：");
-        for (int i = 0; i < 50; i++) {
-            int ii = (int) ((Math.random()) * 101);
-            list.add(ii);
-        }
-        //打印
-        for (int i = 0; i < 50; i++) {
-            System.out.print(list.get(i) + ",");
+    //打印输出排序后数组
+    private static void OutputSortData() {
+        System.out.println("排序后数据为:");
+        Set sortset = map.keySet();
+        Iterator it2 = set.iterator();
+        while (it2.hasNext()) {
+            int key = (Integer) it2.next();
+            System.out.print(key + "=>");
+            int value [] = (int[]) map.get(key);
+            Arrays.sort(value,0,value.length);
+            for (int i = 0; i <value.length ; i++) {
+                if(value[i] != 0){
+                    System.out.print(value[i] + " ");
+                }
+            }
+            System.out.println();
         }
     }
+
 }
