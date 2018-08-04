0729b0126475ab830c8084ec8a267e0a35fe83a0
Wang wuquan <wuquan.wang@hand-china.com>
2018-08-04 14:59:42 +0800

    修改了一次

--- TestProject/src/main/java/ListAndMap.java
+++ TestProject/src/main/java/ListAndMap.java
@@ -10,48 +10,20 @@ import java.util.Set;
  */
 public class ListAndMap {
 
+
+    public  static List<Integer> list = new ArrayList<Integer>();
+    public  static Map<Integer, int[]> map = new HashMap<Integer,int[]>();
+    public  static Set set=null;
     public static void main(String args[]) {
-
-        List<Integer> list = new ArrayList<Integer>();
-        Map<Integer, int[]> map = new HashMap<Integer,int[]>();
         //生成随机数
-        for (int i = 0; i < 50; i++) {
-            int ii = (int) ((Math.random()) * 101);
-            list.add(ii);
-        }
-        System.out.print("随机生成50个小于100的数,分别为：");
-        //打印
-        for (int i = 0; i < 50; i++) {
-            System.out.print(list.get(i) + ",");
-        }
-        //将数组值放入map
-        for (int i = 0; i < 10; i++) {
-            int array[] = new int[10];
-            int num = 0;
-            for (int j = 0; j < 50; j++) {
-                if (list.get(j) / 10 == i) {
-                    array[num] = list.get(j);
-                    num++;
-                }
-            }
-            map.put(i, array);
-        }
-        System.out.println();
-        System.out.print("Map原数据为:");
+        GetRandomNumber();
+        //将数组值放入map并且遍历
+        addItemToMap();
+        //排序并且打印输出
+        OutputSortData();
+    }
 
-        Set set = map.keySet();
-        Iterator it = set.iterator();
-        while (it.hasNext()) {
-            int key = (Integer) it.next();
-            System.out.print(key + "=>");
-            int[] value = (int[]) map.get(key);
-
-            for (int i = 0; i < 10; i++) {
-                if (value[i] != 0)
-                    System.out.print(value[i] + " ");
-            }
-            System.out.println();
-        }
+    private static void OutputSortData() {
         System.out.print("排序后数据为:");
         Set set2 = map.keySet();
         Iterator it2 = set.iterator();
@@ -76,6 +48,47 @@ public class ListAndMap {
             }
             System.out.println();
         }
+    }
 
+    private static void addItemToMap() {
+        for (int i = 0; i < 10; i++) {
+            int array[] = new int[10];
+            int num = 0;
+            for (int j = 0; j < 50; j++) {
+                if (list.get(j) / 10 == i) {
+                    array[num] = list.get(j);
+                    num++;
+                }
+            }
+            map.put(i, array);
+        }
+        System.out.println();
+        System.out.print("Map原数据为:");
+        set = map.keySet();
+        Iterator it = set.iterator();
+        while (it.hasNext()) {
+            int key = (Integer) it.next();
+            System.out.print(key + "=>");
+            int[] value = (int[]) map.get(key);
+
+            for (int i = 0; i < 10; i++) {
+                if (value[i] != 0)
+                    System.out.print(value[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //生成随机数函数
+    private static void GetRandomNumber() {
+        System.out.print("随机生成50个小于100的数,分别为：");
+        for (int i = 0; i < 50; i++) {
+            int ii = (int) ((Math.random()) * 101);
+            list.add(ii);
+        }
+        //打印
+        for (int i = 0; i < 50; i++) {
+            System.out.print(list.get(i) + ",");
+        }
     }
 }
